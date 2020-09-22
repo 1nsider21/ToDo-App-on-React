591a6aedf63779fbe75f52ff813672bd8509e16c
Artur <artroyal.pro@gmail.com>
2020-09-22 23:33:43 +0300

    Updates

---
+++ precache-manifest.039e27aec15562a661ac1e735fb581e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70fe0e75d6f2d718049faceaf9c184c6",
+    "url": "/ToDo-App-on-React/index.html"
+  },
+  {
+    "revision": "e0668af1bb6ac62f294a",
+    "url": "/ToDo-App-on-React/static/css/main.d183c019.chunk.css"
+  },
+  {
+    "revision": "21d064e87514ad7b8f68",
+    "url": "/ToDo-App-on-React/static/js/2.07d3c7f4.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/ToDo-App-on-React/static/js/2.07d3c7f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0668af1bb6ac62f294a",
+    "url": "/ToDo-App-on-React/static/js/main.bb3aa56c.chunk.js"
+  },
+  {
+    "revision": "38e9a6dc2e64c114b1c8",
+    "url": "/ToDo-App-on-React/static/js/runtime-main.0012f33f.js"
+  }
+]);
